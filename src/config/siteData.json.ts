a61641a104b166abc7574a54e4b53c017ef86c4e
rnel <rnelgit@gmail.com>
2025-09-11 18:54:07 +0200

    Update siteData.json.ts

--- src/config/siteData.json.ts
+++ src/config/siteData.json.ts
@@ -18,7 +18,7 @@ export interface SiteDataProps {
 const siteData: SiteDataProps = {
 	name: "Horizon Photography",
 	// Your website's title and description (meta fields)
-	title: "Horizon - a beautiful photography website template made with Astro and Tailwind CSS",
+	title: "Horizon - a beautiful photography website template made with koochiwoochi and Tailwind CSS",
 	description:
 		"Get your next website up and running quickly with our beautiful website theme designed using Astro and Tailwind CSS. Perfect for freelancers, developers, startups, and personal use.",
 	useViewTransitions: true,
